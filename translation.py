9eced69d68de96edf45801c6eb6c73a8e73314fc
Good Boy <78008481+v-v-r-official@users.noreply.github.com>
2021-02-28 09:47:17 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -12,14 +12,12 @@ I Am The Manager Of @VKPROJECTS.You Can Sent FeedBack Through Me."
 
       ABOUT = """**MY DETAILS:**
 
-```🤖My Name:``` [Feedback Bot](https://t.me/F)
+```🤖My Name:``` [Feedback Bot](https://t.me/FeedBACKVKBOT)
     
 ```📝 Language:``` [Python 3](https://www.python.org/)
 
 ```🧰 Framework:``` [Pyrogram](https://github.com/pyrogram/pyrogram)
 
-```👨🏻‍💻 Developer:``` [𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬](https://t.me/Ns_AnoNymouS)
-               
 ```📢 Channel:``` [VK BOT UPDATES](https://t.me/VKPROJECTS)
 
 ```👥 Group:``` [VK BOT SUPPORT](https://t.me/VKP_BOTS)
